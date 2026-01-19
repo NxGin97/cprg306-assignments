ed8de611859f3a01b056fe22c524246bca91644e
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-01-19 13:57:24 -0700

    swapped <main> elements in StudentInfo to <section>

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <main>
+    <section>
       <p>Name: Natalie Ngo</p>
       <p>
       <Link href="https://github.com/NxGin97/cprg306-assignments.git">GitHub: NxGin97/cprg306-assignments</Link>
       </p>
-    </main>
+    </section>
   );
 }
