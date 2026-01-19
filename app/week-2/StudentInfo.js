df8dc00844c4ee2fb5443dc4b12c7c34f210b355
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-01-19 11:45:24 -0700

    shortened url name

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <main>
       <h1>Name: Natalie Ngo</h1>
-      <Link href="https://github.com/NxGin97/cprg306-assignments.git">GitHub: https://github.com/NxGin97/cprg306-assignments.git</Link>
+      <Link href="https://github.com/NxGin97/cprg306-assignments.git">GitHub: NxGin97/cprg306-assignments</Link>
     </main>
   );
 }
