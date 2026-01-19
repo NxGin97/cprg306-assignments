04383a66c61fe1f61ff992b5c58be33c89a98ede
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-01-19 13:39:48 -0700

    readjusting the tags in studentinfo

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <main>
-      <h1>Name: Natalie Ngo</h1>
+      <p>Name: Natalie Ngo</p>
+      <p>
       <Link href="https://github.com/NxGin97/cprg306-assignments.git">GitHub: NxGin97/cprg306-assignments</Link>
+      </p>
     </main>
   );
 }
