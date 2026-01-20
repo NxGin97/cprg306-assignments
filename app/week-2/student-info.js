a35150104f736b31a64954daed8c75eac991825f
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-01-20 10:13:35 -0700

    adding proper link notation to student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,8 +4,8 @@ export default function StudentInfo() {
   return (
     <section>
       <p>Name: Natalie Ngo</p>
-      <p>
-      <Link href="https://github.com/NxGin97/cprg306-assignments.git">GitHub: NxGin97/cprg306-assignments</Link>
+      <p>GitHub:{" "}
+      <Link href="https://github.com/NxGin97/cprg306-assignments.git">NxGin97/cprg306-assignments</Link>
       </p>
     </section>
   );
