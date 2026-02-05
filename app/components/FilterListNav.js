dfea73deac57522a18747b87bfac6c5c5477cd25
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-02-05 10:30:18 -0700

    renaming components in filter list nav and also simplified some styling

--- app/components/FilterListNav.js
+++ app/components/FilterListNav.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-
-//overall flex layout
+//Im sure theres probably a simpler way to do the toggle using active but my brain just can't
+//overall flex layout 
 export default function FilterListById() {
   return(
     <div className="mt-5 max-w-[850px] mx-auto">
@@ -8,8 +8,8 @@ export default function FilterListById() {
         <p className="font-bold text-2xl opacity-80">Sort by:</p>
         <nav>
           <ul className="flex gap-2">
-            <FilterListNavSelected title="Id" pageLink="week-4" />
-            <FilterListNav title="Category" pageLink="week-4a" />
+            <SortTabActive title="Id" pageLink="week-4" />
+            <SortTab title="Category" pageLink="week-4a" />
           </ul>
         </nav>
       </div>
@@ -24,8 +24,8 @@ export function FilterListByCategory() {
         <p className="font-bold text-2xl opacity-80">Sort by:</p>
         <nav>
           <ul className="flex gap-2">
-            <FilterListNav title="Id" pageLink="week-4" />
-            <FilterListNavSelected title="Category" pageLink="week-4a" />
+            <SortTab title="Id" pageLink="week-4" />
+            <SortTabActive title="Category" pageLink="week-4a" />
           </ul>
         </nav>
       </div>
@@ -33,24 +33,26 @@ export function FilterListByCategory() {
   )
 }
 
+const buttonStyle = "w-[120px] m-1 px-3 py-2 rounded-md text-center text-base text-violet-100 shadow-xl/20"
+
 //component 'buttons'
-export function FilterListNav({ title, pageLink }) {
+export function SortTab({ title, pageLink }) {
   return (
-    <li className=" w-[120px] bg-white opacity-70 m-1 px-3 py-2 rounded-md text-center text-base text-violet-100 shadow-xl/20 hover:bg-violet-200 hover:opacity-90 ">
+    <li className={`${buttonStyle} bg-white opacity-70  hover:bg-violet-200 hover:opacity-90`}>
       <Link
         href={pageLink}
-        className="block w-full h-full text-violet-800 text-md"> {title}
+        className="block w-full h-full text-violet-800 text-md cursor-pointer"> {title}
       </Link>
     </li>
   );
 }
 
-export function FilterListNavSelected({ title, pageLink }) {
+export function SortTabActive({ title, pageLink }) {
   return (
-    <li className=" w-[120px] bg-violet-800 opacity-75 m-1 px-3 py-2 rounded-md text-center text-base text-violet-100 shadow-xl/20">
+    <li className={`${buttonStyle} bg-violet-800 opacity-75`}>
       <Link
         href={pageLink}
-        className="block w-full h-full text-white text-md">{title}
+        className="block w-full h-full text-white text-md cursor-pointer">{title}
         </Link>
     </li>
   );
