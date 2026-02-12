243e0ddfcb25703a54f4b6600081b0f4a3e20684
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-02-12 12:53:11 -0700

    chagning empty page background color

--- app/empty-page/page.js
+++ app/empty-page/page.js
@@ -2,7 +2,7 @@ import ReturnHome from '../components/ReturnHome';
 
 export default function Page() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center font-size-30">
+    <main className="min-h-screen flex flex-col items-center justify-center font-size-30 bg-violet-100 py-10">
       <h1 className="font-bold text-center">Page is not ready to view...</h1>
       <ReturnHome />
     </main>
